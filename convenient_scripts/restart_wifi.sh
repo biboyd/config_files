1360e56b41ddac7788d82c80142cc3446e085c40
biboyd <brendanboyd16@gmail.com>
2022-05-24 17:20:58 -0400

    add retries to connect

--- convenient_scripts/restart_wifi.sh
+++ convenient_scripts/restart_wifi.sh
@@ -10,3 +10,11 @@ echo "Reconnecting to $network"
 iwctl station wlan0 disconnect
 sleep 3.2
 iwctl station wlan0 connect $network
+sleep 3.2
+newid=`iwgetid --raw`
+while [ "$newid" != "$network" ]
+do
+	iwctl station wlan0 connect $network
+	sleep 3.2
+	newid=`iwgetid --raw`
+done
